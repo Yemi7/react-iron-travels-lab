f0d6c689a08dff81eb59b063e7b2e7ad14ea89f4
yemi <o.ogunbadejo7@gmail.com>
2026-05-27 18:01:24 +0100

    Solved Lab, not 5 bonus

--- src/components/TravelPlanCard.jsx
+++ src/components/TravelPlanCard.jsx
@@ -9,25 +9,34 @@ export function TravelPlanCard(props) {
         setCurrentList(newList);
     }
     return (
-        <div>
-            {currentList.map((locationObj) => {
-                return (
-                    <div key={locationObj.id} className='location-container'>
-                        <img src={locationObj.image} className='image' />
-                        <div className='info'>
-                            <h3>{locationObj.destination} ({locationObj.days} Days)</h3>
-                            <p><em>{locationObj.description}</em></p>
-                            <p><strong>Price:</strong> €{locationObj.totalCost}</p>
-                            <aside className='label-container'>
-                                {locationObj.totalCost <= 350 && <span className='label great-deal'>Great Deal</span>}
-                                {locationObj.totalCost >= 1500 && <span className='label premium'>Premium</span>}
-                                {locationObj.allInclusive && <span className='label all-inclusive'>All-Inclusive</span>}
-                            </aside>
-                            <button onClick={() => { deleteButton(locationObj.id) }} className='delete-button'>Delete</button>
+        <div className='main-flex'>
+            <div className='list'>
+                {currentList.map((locationObj) => {
+                    return (
+                        <div key={locationObj.id} className='location-container'>
+                            <img src={locationObj.image} className='image' />
+                            <div className='info'>
+                                <h3>{locationObj.destination} ({locationObj.days} Days)</h3>
+                                <p><em>{locationObj.description}</em></p>
+                                <p><strong>Price:</strong> €{locationObj.totalCost}</p>
+                                <aside className='label-container'>
+                                    {locationObj.totalCost <= 350 && <span className='label great-deal'>Great Deal</span>}
+                                    {locationObj.totalCost >= 1500 && <span className='label premium'>Premium</span>}
+                                    {locationObj.allInclusive && <span className='label all-inclusive'>All-Inclusive</span>}
+                                </aside>
+                                <button onClick={() => { deleteButton(locationObj.id) }} className='delete-button'>Delete</button>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
+            <div className='favs'>
+                <h3>Favourites</h3>
+                <div className='favs-container'>
+                    <img src={props.plan[0].image} className='favs-image' />
+                </div>
+            </div>
+
         </div>
     )
 }
