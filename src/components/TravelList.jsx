07bfc702d2514d91244b94634c8e02f32a7a5c1d
yemi <o.ogunbadejo7@gmail.com>
2026-05-27 17:37:55 +0100

    refactored to have each card in child component

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,34 +1,15 @@
 import travelPlansData from '../assets/travel-plans.json';
 import { useState } from 'react';
+import { TravelPlanCard } from './TravelPlanCard';
+
 
 export function TravelList() {
-    const [currentList, setCurrentList] = useState(travelPlansData);
-    const deleteButton = (id) => {
-    const newList = currentList.filter((locationObj) => locationObj.id !== id);
-    setCurrentList(newList);
-    }
 
 
-    return (
-        <div>
-            {currentList.map((locationObj) => {
-                return (
-                    <div key={locationObj.id} className='location-container'>
-                        <img src={locationObj.image} className='image' />
-                        <div className='info'>
-                            <h3>{locationObj.destination} ({locationObj.days} Days)</h3>
-                            <p><em>{locationObj.description}</em></p>
-                            <p><strong>Price:</strong> €{locationObj.totalCost}</p>
-                            <aside className='label-container'>
-                                {locationObj.totalCost <= 350 && <span className='label great-deal'>Great Deal</span>}
-                                {locationObj.totalCost >= 1500 && <span className='label premium'>Premium</span>}
-                                {locationObj.allInclusive && <span className='label all-inclusive'>All-Inclusive</span>}
-                            </aside>
-                            <button onClick={() => { deleteButton(locationObj.id) }} className='delete-button'>Delete</button>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
+    return(
+
+        <TravelPlanCard plan={travelPlansData}/>
     )
+
+
 }
--- src/components/TravelPlanCard.jsx
+++ src/components/TravelPlanCard.jsx
@@ -1,3 +1,33 @@
-export function TravelPlanCard() {
-    
+
+import { useState } from 'react';
+
+
+export function TravelPlanCard(props) {
+    const [currentList, setCurrentList] = useState(props.plan);
+    const deleteButton = (id) => {
+        const newList = currentList.filter((locationObj) => locationObj.id !== id);
+        setCurrentList(newList);
+    }
+    return (
+        <div>
+            {currentList.map((locationObj) => {
+                return (
+                    <div key={locationObj.id} className='location-container'>
+                        <img src={locationObj.image} className='image' />
+                        <div className='info'>
+                            <h3>{locationObj.destination} ({locationObj.days} Days)</h3>
+                            <p><em>{locationObj.description}</em></p>
+                            <p><strong>Price:</strong> €{locationObj.totalCost}</p>
+                            <aside className='label-container'>
+                                {locationObj.totalCost <= 350 && <span className='label great-deal'>Great Deal</span>}
+                                {locationObj.totalCost >= 1500 && <span className='label premium'>Premium</span>}
+                                {locationObj.allInclusive && <span className='label all-inclusive'>All-Inclusive</span>}
+                            </aside>
+                            <button onClick={() => { deleteButton(locationObj.id) }} className='delete-button'>Delete</button>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
